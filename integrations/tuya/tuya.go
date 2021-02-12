4eedcceee921f804d863e23029e9cdc64412f579
SMerrony <merrony@gmail.com>
2021-02-12 14:02:56 +0100

    Extend Tuya to support Sockets (LIDL ZigBee...)

--- integrations/tuya/tuya.go
+++ integrations/tuya/tuya.go
@@ -45,13 +45,14 @@ const (
 
 // The Tuya type encapsulates the Tuya IoT Integration
 type Tuya struct {
-	conf         confT
-	evChan       chan events.EventT
-	mqttChan     chan mqtt.MessageT
-	stopChans    []chan bool // used for stopping Goroutines
-	mq           mqtt.MQTT
-	tuyaMu       sync.RWMutex
-	lampsByLabel map[string]int
+	conf           confT
+	evChan         chan events.EventT
+	mqttChan       chan mqtt.MessageT
+	stopChans      []chan bool // used for stopping Goroutines
+	mq             mqtt.MQTT
+	tuyaMu         sync.RWMutex
+	lampsByLabel   map[string]int
+	socketsByLabel map[string]int
 }
 
 // confT fields exported for unmarshalling
@@ -60,6 +61,7 @@ type confT struct {
 	ApiKey     string
 	TuyaRegion string
 	Lamp       []lamp
+	Socket     []socket
 }
 
 type lamp struct {
@@ -83,12 +85,26 @@ type hsvT struct {
 	H, S, V int
 }
 
+type socket struct {
+	DeviceID string
+	Label    string
+	status   socketStatusT
+}
+
+type socketStatusT struct {
+	Switch1     bool
+	Countdown1  float64
+	RelayStatus string
+	LightMode   string
+}
+
 // LoadConfig loads and stores the configuration for this Integration
 func (t *Tuya) LoadConfig(confdir string) error {
 	t.tuyaMu.Lock()
 	defer t.tuyaMu.Unlock()
 	confBytes, err := agconfig.PreprocessTOML(confdir, configFilename)
 	t.lampsByLabel = make(map[string]int)
+	t.socketsByLabel = make(map[string]int)
 	if err != nil {
 		log.Fatalf("ERROR: Could not read Tuya config due to %s\n", err.Error())
 	}
@@ -99,9 +115,15 @@ func (t *Tuya) LoadConfig(confdir string) error {
 	}
 	log.Printf("DEBUG: Tuya config is... %v\n", t.conf)
 	if len(t.conf.Lamp) > 0 {
-		log.Printf("INFO: Tuya Integration has %d lamps configured\n", len(t.conf.Lamp))
+		log.Printf("INFO: Tuya Integration has %d lamp(s) configured\n", len(t.conf.Lamp))
 		for ix, l := range t.conf.Lamp {
 			t.lampsByLabel[l.Label] = ix
+		}
+	}
+	if len(t.conf.Socket) > 0 {
+		log.Printf("INFO: Tuya Integration has %d socket(s) configured\n", len(t.conf.Socket))
+		for ix, s := range t.conf.Socket {
+			t.socketsByLabel[s.Label] = ix
 		}
 	}
 	return nil
@@ -109,7 +131,7 @@ func (t *Tuya) LoadConfig(confdir string) error {
 
 // ProvidesDeviceTypes returns a slice of device types that this Integration supplies.
 func (t *Tuya) ProvidesDeviceTypes() []string {
-	return []string{"Lamp"}
+	return []string{"Lamp", "Socket"}
 }
 
 // Start launches the Integration, LoadConfig() should have been called beforehand.
@@ -136,6 +158,7 @@ func (t *Tuya) Start(evChan chan events.EventT, mq mqtt.MQTT) {
 	go t.monitorClients()
 	go t.monitorActions()
 	go t.monitorLamps()
+	go t.monitorSockets()
 }
 
 func (t *Tuya) addStopChan() (ix int) {
@@ -179,67 +202,86 @@ func (t *Tuya) monitorClients() {
 			payload := string(msg.Payload.([]uint8))
 			topicSlice := strings.Split(msg.Topic, "/")
 			t.tuyaMu.RLock()
-			ix, found := t.lampsByLabel[topicSlice[3]]
-			if !found {
-				log.Printf("WARNING: Tuya front-end monitor got command for unknown unit <%s>\n", topicSlice[3])
-				t.tuyaMu.RUnlock()
-				continue
-			}
-			control := topicSlice[4]
-
-			log.Printf("DEBUG: Tuya got control %s for %s with payload %s\n", control, t.conf.Lamp[ix].Label, payload)
-			var code, code2 string
-			var value, value2 interface{}
-			switch control {
-			case "switch":
-				switch payload {
-				case "Off":
-					code = "switch_led"
-					value = false
-				case "White":
-					code = "switch_led"
-					value = true
-					code2 = "work_mode"
-					value2 = "white"
-				case "Colour":
-					code = "switch_led"
-					value = true
-					code2 = "work_mode"
-					value2 = "colour"
-				}
-			case "switch_led":
-				code = "switch_led"
-				value = payload == "true" // bool
-			case "colour_data_v2":
-				code = "colour_data_v2"
-				var cd clientHSV
-				err := json.Unmarshal([]byte(payload), &cd)
-				if err != nil {
-					log.Printf("WARNING: Tuya could not unmarshal HSV from client - %s\n", err.Error())
+			var ix int
+			var foundLamp, foundSocket bool
+			ix, foundLamp = t.lampsByLabel[topicSlice[3]]
+			if !foundLamp {
+				ix, foundSocket = t.socketsByLabel[topicSlice[3]]
+				if !foundSocket {
+					log.Printf("WARNING: Tuya front-end monitor got command for unknown unit <%s>\n", topicSlice[3])
 					t.tuyaMu.RUnlock()
 					continue
 				}
-				log.Printf("DEBUG: Tuya - H: %f, S: %f, V: %f\n", cd.H, cd.S, cd.V)
-				value = fmt.Sprintf("{\"h\":%d,\"s\":%d,\"v\":%d}", int(cd.H), int(cd.S*1000.0), int(cd.V*1000.0))
-				log.Printf("DEBUG: ... encoding to %s\n", value)
-			case "bright_value_v2":
-				code = "bright_value_v2"
-				value, _ = strconv.Atoi(payload)
-			case "temp_value_v2":
-				code = "temp_value_v2"
-				value, _ = strconv.Atoi(payload)
 			}
-			log.Printf("DEBUG: Tuya sending Code: %s, Value: %v\n", code, value)
-			var err error
-			if code2 == "" {
-				_, err = device.PostDeviceCommand(t.conf.Lamp[ix].DeviceID, []device.Command{{Code: code, Value: value}})
-			} else {
-				_, err = device.PostDeviceCommand(t.conf.Lamp[ix].DeviceID, []device.Command{{Code: code, Value: value}, {Code: code2, Value: value2}})
+			control := topicSlice[4]
+			if foundLamp {
+				log.Printf("DEBUG: Tuya got control %s for %s with payload %s\n", control, t.conf.Lamp[ix].Label, payload)
+				var code, code2 string
+				var value, value2 interface{}
+				switch control {
+				case "switch":
+					switch payload {
+					case "Off":
+						code = "switch_led"
+						value = false
+					case "White":
+						code = "switch_led"
+						value = true
+						code2 = "work_mode"
+						value2 = "white"
+					case "Colour":
+						code = "switch_led"
+						value = true
+						code2 = "work_mode"
+						value2 = "colour"
+					}
+				case "switch_led":
+					code = "switch_led"
+					value = payload == "true" // bool
+				case "colour_data_v2":
+					code = "colour_data_v2"
+					var cd clientHSV
+					err := json.Unmarshal([]byte(payload), &cd)
+					if err != nil {
+						log.Printf("WARNING: Tuya could not unmarshal HSV from client - %s\n", err.Error())
+						t.tuyaMu.RUnlock()
+						continue
+					}
+					log.Printf("DEBUG: Tuya - H: %f, S: %f, V: %f\n", cd.H, cd.S, cd.V)
+					value = fmt.Sprintf("{\"h\":%d,\"s\":%d,\"v\":%d}", int(cd.H), int(cd.S*1000.0), int(cd.V*1000.0))
+					log.Printf("DEBUG: ... encoding to %s\n", value)
+				case "bright_value_v2":
+					code = "bright_value_v2"
+					value, _ = strconv.Atoi(payload)
+				case "temp_value_v2":
+					code = "temp_value_v2"
+					value, _ = strconv.Atoi(payload)
+				}
+				log.Printf("DEBUG: Tuya sending Code: %s, Value: %v\n", code, value)
+				var err error
+				if code2 == "" {
+					_, err = device.PostDeviceCommand(t.conf.Lamp[ix].DeviceID, []device.Command{{Code: code, Value: value}})
+				} else {
+					_, err = device.PostDeviceCommand(t.conf.Lamp[ix].DeviceID, []device.Command{{Code: code, Value: value}, {Code: code2, Value: value2}})
+				}
+				if err != nil {
+					log.Printf("WARNING: Tuya Integration got error sending command - %s\n", err.Error())
+					t.tuyaMu.RUnlock()
+					continue
+				}
 			}
-			if err != nil {
-				log.Printf("WARNING: Tuya Integration got error sending command - %s\n", err.Error())
-				t.tuyaMu.RUnlock()
-				continue
+			if foundSocket {
+				log.Printf("DEBUG: Tuya got control %s for %s with payload %s\n", control, t.conf.Socket[ix].Label, payload)
+				value := false
+				if payload == "On" {
+					value = true
+				}
+				_, err := device.PostDeviceCommand(t.conf.Socket[ix].DeviceID, []device.Command{{Code: "switch_1", Value: value}})
+				if err != nil {
+					log.Printf("WARNING: Tuya Integration got error sending command - %s\n", err.Error())
+					t.tuyaMu.RUnlock()
+					continue
+				}
 			}
 			t.tuyaMu.RUnlock()
 		}
@@ -259,7 +301,7 @@ func (t *Tuya) monitorLamps() {
 			if err != nil {
 				log.Printf("WARNING: Tuya GetDeviceStatus failed with %s\n", err.Error())
 			} else {
-				log.Printf("DEBUG: Tuya device status response Code: %d, Message: %s, Success: %v\n", status.Code, status.Msg, status.Success)
+				// log.Printf("DEBUG: Tuya device status response Code: %d, Message: %s, Success: %v\n", status.Code, status.Msg, status.Success)
 				if status.Success {
 					var currentStatus lampStatusT
 					for _, r := range status.Result {
@@ -307,6 +349,66 @@ func (t *Tuya) monitorLamps() {
 	}
 }
 
+// monitorSockets
+func (t *Tuya) monitorSockets() {
+	sc := t.addStopChan()
+	t.tuyaMu.RLock()
+	stopChan := t.stopChans[sc]
+	t.tuyaMu.RUnlock()
+	everyMinute := time.NewTicker(time.Minute)
+	for {
+		for _, socket := range t.conf.Socket {
+			status, err := device.GetDeviceStatus(socket.DeviceID)
+			if err != nil {
+				log.Printf("WARNING: Tuya GetDeviceStatus failed with %s\n", err.Error())
+			} else {
+				// log.Printf("DEBUG: Tuya device status response Code: %d, Message: %s, Success: %v\n", status.Code, status.Msg, status.Success)
+				if status.Success {
+					var currentStatus socketStatusT
+					for _, r := range status.Result {
+						// log.Printf("DEBUG: ... Code: %s, Value: %v\n", r.Code, r.Value)
+						switch r.Code {
+						case "switch_1":
+							currentStatus.Switch1 = r.Value.(bool)
+						case "countdown_1":
+							currentStatus.Countdown1 = r.Value.(float64)
+						case "relay_status":
+							currentStatus.RelayStatus = r.Value.(string)
+						case "light_mode":
+							currentStatus.LightMode = r.Value.(string)
+						}
+					}
+					t.tuyaMu.Lock()
+					socket.status = currentStatus
+					t.tuyaMu.Unlock()
+					// log.Printf("DEBUG: ... current Status: %v\n", currentStatus)
+					payload, err := json.Marshal(currentStatus)
+					if err != nil {
+						log.Fatalf("ERROR: Tuya could not marshal status info - %s\n", err.Error())
+					}
+					// log.Println("DEBUG: Tuya - sending MQTT update...")
+					t.mqttChan <- mqtt.MessageT{
+						Topic:    mqttPrefix + socket.Label + "/status",
+						Qos:      0,
+						Retained: false,
+						Payload:  payload,
+					}
+				}
+			}
+		}
+		select {
+		case <-stopChan:
+			return
+		case <-everyMinute.C:
+			continue
+		}
+	}
+}
+
+func getDeviceName(evName string) string {
+	return strings.Split(evName, "/")[events.EvDeviceName]
+}
+
 // monitorActions listens for Control Actions from Automations and performs them
 func (t *Tuya) monitorActions() {
 	sc := t.addStopChan()
@@ -324,11 +426,37 @@ func (t *Tuya) monitorActions() {
 			return
 		case ev := <-ch:
 			log.Printf("DEBUG: Tuya Action Monitor got %v\n", ev)
-			// lampIx, found := t.lampsByLabel[ev.DeviceName]
-			// if !found {
-			// 	log.Printf("WARNING: Tuya automation got Action for unknown unit <%s>\n", ev.DeviceName)
-			// 	continue
-			// }
+			var ix int
+			var foundLamp, foundSocket bool
+			ix, foundLamp = t.lampsByLabel[getDeviceName(ev.Name)]
+			if !foundLamp {
+				ix, foundSocket = t.socketsByLabel[getDeviceName(ev.Name)]
+			}
+			switch {
+			case foundLamp:
+				log.Println("WARNING: Tuya Integration does not yet support Lamp Automation Actions")
+			case foundSocket:
+				control := strings.Split(ev.Name, "/")[events.EvControl]
+				switch control {
+				case "power":
+					value := false
+					if ev.Value.(string) == "on" {
+						value = true
+					}
+					_, err := device.PostDeviceCommand(t.conf.Socket[ix].DeviceID, []device.Command{{Code: "switch_1", Value: value}})
+					if err != nil {
+						log.Printf("WARNING: Tuya Integration got error sending command - %s\n", err.Error())
+						t.tuyaMu.RUnlock()
+						continue
+					}
+				default:
+					log.Printf("WARNING: Tuya Action got unknown control <%s>\n", control)
+				}
+			default:
+				log.Printf("WARNING: Tuya Action monitor got command for unknown unit <%s>\n", getDeviceName(ev.Name))
+				t.tuyaMu.RUnlock()
+				continue
+			}
 
 		}
 	}
