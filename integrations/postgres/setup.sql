dba1b9349f774ada484c019416fd4d3a4188e2aa
SMerrony <merrony@gmail.com>
2021-02-19 10:46:10 +0100

    Use time zone in Postgres scripts

--- integrations/postgres/setup.sql
+++ integrations/postgres/setup.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS logged_integers;
 CREATE TABLE logged_integers
 (
     id INTEGER REFERENCES names(id),
-    ts TIMESTAMP NOT NULL,
+    ts TIMESTAMP WITH TIME ZONE NOT NULL,
     int_val BIGINT NOT NULL,
     CONSTRAINT ints_pkey PRIMARY KEY (id, ts)
 );
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS logged_floats;
 CREATE TABLE logged_floats
 (
     id INTEGER REFERENCES names(id),
-    ts TIMESTAMP NOT NULL,
+    ts TIMESTAMP WITH TIME ZONE NOT NULL,
     float_val FLOAT NOT NULL,
     CONSTRAINT floats_pkey PRIMARY KEY (id, ts)
 );
@@ -43,7 +43,7 @@ DROP TABLE IF EXISTS logged_strings;
 CREATE TABLE logged_strings
 (
     id INTEGER REFERENCES names(id),
-    ts TIMESTAMP NOT NULL,
+    ts TIMESTAMP WITH TIME ZONE NOT NULL,
     string_val TEXT NOT NULL,
     CONSTRAINT strings_pkey PRIMARY KEY (id, ts)
 );
